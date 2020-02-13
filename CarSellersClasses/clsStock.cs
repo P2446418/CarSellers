7118765c7ef6a36095e0753e19f6fa360fc5c250
Benjamin Taylor <P2447144@my365.dmu.ac.uk>
2020-02-13 10:56:29 +0000

    Stock testing done

--- CarSellersClasses/clsStock.cs
+++ CarSellersClasses/clsStock.cs
@@ -1,7 +1,13 @@
-﻿namespace CarSellersClasses
+﻿using System;
+namespace CarSellersClasses
 {
     public class clsStock
     {
         public string numberPlate;
+        public DateTime productionYear;
+        public int mileage;
+        public double price;
+        public bool sold;
+        public string modelName;
     }
 }
