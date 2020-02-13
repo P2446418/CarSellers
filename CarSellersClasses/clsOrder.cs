09f80bd0211aded1cf182d946804e8bdf34ae6df
Matthew Susans <p2446418@my365.dmu.ac.uk>
2020-02-13 10:58:09 +0000

    Added fields and more tests for Order class

--- CarSellersClasses/clsOrder.cs
+++ CarSellersClasses/clsOrder.cs
@@ -1,6 +1,14 @@
-﻿namespace CarSellersClasses
+﻿using System;
+
+namespace CarSellersClasses
 {
     public class clsOrder
     {
+        public int orderID; //done
+        public string numberPlate; //tested
+        public int customerID; //tested
+        public int quantity; //tested
+        public double price;
+        public DateTime dateOrdered; //tested
     }
 }
--- CarSellersTesting/OrderTest.cs
+++ CarSellersTesting/OrderTest.cs
@@ -12,5 +12,60 @@ namespace CarSellersTesting
             clsOrder anOrder = new clsOrder();
             Assert.IsNotNull(anOrder);
         }
+
+        [TestMethod]
+        public void OrderIDPropertyOK()
+        {
+            clsOrder new_order = new clsOrder();
+            int TestData = 1;
+            new_order.orderID = TestData;
+            Assert.AreEqual(new_order.orderID, TestData);
+        }
+
+        [TestMethod]
+        public void DateOrderedPropertyOK()
+        {
+            clsOrder new_order = new clsOrder();
+            DateTime test_data = DateTime.Now.Date;
+            new_order.dateOrdered = test_data;
+            Assert.AreEqual(new_order.dateOrdered, test_data);
+
+        }
+
+        [TestMethod]
+        public void NumberPlatePropertyOK()
+        {
+            clsOrder new_order = new clsOrder();
+            String test_data = "YS12ASK";
+            new_order.numberPlate = test_data;
+            Assert.AreEqual(new_order.numberPlate, test_data);
+        }
+
+        [TestMethod]
+        public void CustomerIDPropertyOK()
+        {
+            clsOrder new_order = new clsOrder();
+            int test_data = 2;
+            new_order.customerID = test_data;
+            Assert.AreEqual(new_order.customerID, test_data);
+        }
+
+        [TestMethod]
+        public void QuantityPropertyOK()
+        {
+            clsOrder new_order = new clsOrder();
+            int test_data = 3;
+            new_order.quantity = test_data;
+            Assert.AreEqual(new_order.quantity, test_data);
+        }
+
+        [TestMethod]
+        public void PricePropertyOK()
+        {
+            clsOrder new_order = new clsOrder();
+            double test_data = 15000.00;
+            new_order.price = test_data;
+            Assert.AreEqual(new_order.price, test_data);
+        }
     }
 }
