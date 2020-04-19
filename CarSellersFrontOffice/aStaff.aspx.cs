79ffc286e2931cb6c002e94db3a939ed711cb7cb
Louis Timberlake <P2447869@my365.dmu.ac.uk>
2020-04-19 18:17:25 +0100

    Updated Staff Layer

--- CarSellersFrontOffice/aStaff.aspx.cs
+++ CarSellersFrontOffice/aStaff.aspx.cs
@@ -3,7 +3,6 @@ using System;
 
 public partial class AStaff : System.Web.UI.Page
 {
-
     protected void Page_Load(object sender, EventArgs e)
     {
         clsStaff aStaff = new clsStaff();
@@ -17,17 +16,44 @@ public partial class AStaff : System.Web.UI.Page
 
         }
     }
-        protected void StaffOKButton_Click(object sender, EventArgs e)
+    protected void StaffOKButton_Click(object sender, EventArgs e)
+    {
+        clsStaff aStaff = new clsStaff();
+        //validation
+        String FirstName = txtFirstName.Text;
+        String Surname = SurnameTextBox.Text;
+        String DOB = DOBTextBox.Text;
+        String MobileNumber = MobNumTextBox.Text;
+        String Position = PosTextBox.Text;
+
+        String error = aStaff.Valid(FirstName, Surname, DOB, MobileNumber, Position);
+
+        if (error == "")
         {
-            clsStaff aStaff = new clsStaff();
-            aStaff.StaffID = txtStaffID.Text;
-            aStaff.FirstName = txtFirstName.Text;
-        
-            //aStaff.Surname = SurnameTextBox.Text;
-            //aStaff.DOB = DOBTextBox.Text;
-            //aStaff.MobileNumber = MobNumTextBox.Text;
-        
+            //no errors, program will work as normal
+
+            //StaffID set to blank to prevent error, ID allocated by db
+            aStaff.StaffID = 0;
+            aStaff.FirstName = FirstName;
+            aStaff.Surname = Surname;
+            aStaff.DOB = DateTime.Parse(DOB);
+            aStaff.MobileNumber = Convert.ToInt32(MobileNumber);
+            aStaff.Position = Position;
+            //aStaff.toDelete = DelStaff.Checked;
             Session["aStaff"] = aStaff;
             Response.Redirect("StaffViewer.aspx");
+
         }
-}
+        else
+        {
+            //make error box visible, set to size and enter data
+            errorOutput.Visible = true;
+            String[] errors = error.Split('\n');
+            errorOutput.Height = errors.Length * 25;
+            errorOutput.Text = "Errors found within input: " + "\r\n\n";
+            foreach (String errorCase in errors)
+            {
+                errorOutput.Text += errorCase + "\r\n";
+            }
+        }
+    }
