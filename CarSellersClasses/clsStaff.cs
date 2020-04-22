a8466455c82ec8898bc0f16cd789bb6c28ef73b1
Louis Timberlake <P2447869@my365.dmu.ac.uk>
2020-04-22 16:08:03 +0100

    Added StaffCollection and made changes to clsStaff.

--- CarSellersClasses/clsStaff.cs
+++ CarSellersClasses/clsStaff.cs
@@ -1,15 +1,187 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace CarSellersClasses
 {
     public class clsStaff
     {
         public bool InOffice { get; set; }
-        public int StaffID { get; set; }
-        public string FirstName { get; set; }
-        public string Surname { get; set; }
-        public string MobileNumber { get; set; }
-        public DateTime DOB { get; set; }
-        public string Position { get; set; }
+
+        #region ClassFields
+
+        private int mStaffID;
+        public int StaffID
+        {
+            get { return mStaffID; }
+            set { mStaffID = value; }
+        }
+
+        private String mFirstName;
+        public String FirstName
+        {
+            get { return mFirstName; }
+            set { mFirstName = value; }
+        }
+
+        private String mSurname;
+        public String Surname
+        {
+            get { return mSurname; }
+            set { mSurname = value; }
+        }
+
+        private int mMobileNumber;
+        public int MobileNumber
+        {
+            get { return mMobileNumber; }
+            set { mMobileNumber = value; }
+        }
+
+        private DateTime mDOB;
+        public DateTime DOB
+        {
+            get { return mDOB; }
+            set { mDOB = value; }
+        }
+
+        private String mPosition;
+        public String Position
+        {
+            get { return mPosition; }
+            set { mPosition = value; }
+        }
+
+        private Boolean mDelete;
+        public Boolean toDelete
+        {
+            get { return mDelete; }
+            set { mDelete = value; }
+        }
+
+        #endregion
+
+        //find method
+        //currently uses the filterById procedure, not sure if that will change in future
+        public bool Find(int StaffNo)
+        {
+            //create an instance of the data connection
+            clsDataConnection Database = new clsDataConnection();
+
+            //add the parameter for the customer ID to search for
+            Database.AddParameter("@StaffId", StaffNo);
+
+            //execute stored proc
+            Database.Execute("sproc_StaffTable_FilterByID");
+
+            //if one record is found then retrieve it
+            if (Database.Count == 1)
+            {
+                mStaffID = Convert.ToInt32(Database.DataTable.Rows[0]["StaffId"]);
+                mFirstName = Convert.ToString(Database.DataTable.Rows[0]["FirstName"]);
+                mSurname = Convert.ToString(Database.DataTable.Rows[0]["Surname"]);
+                mDOB = DateTime.Parse(Convert.ToString(Database.DataTable.Rows[0]["DateOfBirth"]));
+                mMobileNumber = Convert.ToInt32(Database.DataTable.Rows[0]["MobileNumber"]);
+                mDelete = Convert.ToBoolean(Database.DataTable.Rows[0]["toDelete"]);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Main validation method and sub-validation methods
+        //just simple sanitation and filtering, returns error message if any
+        public String Valid(String SFirstName, String SSurname, String SDOB, String SNumber)
+        {
+            String Error = "";
+            //mName, check for symbols and length
+            Error = ValidateNames(SFirstName, "FirstName");
+            //mSurname, check for symbols and length
+            Error += ValidateNames(SSurname, "SurName");
+            //mDob, check for datetime format and if it's valid for SQL
+            Error += ValidateDateTime(SDOB);
+            //mMobileNumber, check if actual number, no letters
+            Error += ValidateMobileNumber(SNumber);
+            return Error;
+        }
+
+        public String ValidateNames(String FirstNameString, String field)
+        {
+            String error = "";
+            //check for blanks
+            if ((FirstNameString.Length == 0) || (FirstNameString == null))
+            {
+                return "[!] " + field + " cannot be null\n";
+            }
+            //check for out of boundary
+            if (FirstNameString.Length > 25)
+            {
+                return "[!] " + field + " cannot be more than 25 characters\n";
+            }
+            //regex check to look for invalid characters
+            Regex NameCheck = new Regex("[a-zA-z- '-]");
+            if (!(NameCheck.IsMatch(FirstNameString)))
+            {
+                error += "[!] " + field + " contains invalid characters, numbers or symbols\n";
+            }
+
+            return error;
+        }
+
+        public String ValidateDateTime(String date)
+        {
+            //Customer SQL table is using Date data type, so format required is YYYY/MM/DD
+            //C# datetime uses DD/MM/YY HH/MM/SS and can be parsed as different formats
+            String error = "";
+            try
+            {
+                //parse string into datetime and back
+                DateTime tempDate = DateTime.Parse(date);
+                String correctFormat = tempDate.ToString();
+
+                //check if correct format, length etc.
+                if (date != correctFormat) { error += "[!] Date is not in correct format\n"; }
+            }
+            catch //date is not valid date
+            {
+                if (date == "" || date == null)
+                {
+                    return "[!] Date of birth cannot be left blank\n";
+                }
+                else
+                {
+                    error += "[!] Date contains invalid characters and or symbols\n";
+                }
+            }
+
+            return error;
+        }
+
+        public String ValidateMobileNumber(String MobileNumberString)
+        {
+            String error = "";
+            //check for blank input
+            if (MobileNumberString == "" || MobileNumberString == null)
+            {
+                return "[!] Phone number cannot be left blank\n";
+            }
+            try
+            {
+                //convert NumberString into integer
+                String tempString = MobileNumberString.Replace(" ", "1");
+                int testMobileNumber = Convert.ToInt32(tempString);
+            }
+            catch (OverflowException e) //if number exceeds integer size limit
+            {
+                error += "[!] Mobile number is too long\n";
+            }
+            catch //if number cannot be parsed as an integer
+            {
+                error += "[!] Mobile number cannot contain letters or invalid characters\n";
+            }
+            return error;
+        }
+
     }
 }
---
+++ CarSellersClasses/clsStaffCollection.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace CarSellersClasses
+{
+    public class clsStaffCollection
+    {
+        //will handle the adding, editing and deleting of customer instances
+        //customer will only be able to add their own instance
+        //customer will only be able to edit/modify their own instance
+        //customer will only be able to delete their own instance
+        // - which just sets toDelete to true, doesn't delete straight away in case there are pending orders
+        private clsStaff thisStaff;
+
+        public clsStaff getStaff()
+        {
+            return thisStaff;
+        }
+
+        public void setStaff(clsStaff aStaff)
+        {
+            thisStaff = aStaff;
+        }
+
+        public int add(clsStaff aStaff)
+        {
+            clsDataConnection database = new clsDataConnection();
+            database.AddParameter("@FirstName", thisStaff.FirstName);
+            database.AddParameter("@Surname", thisStaff.Surname);
+            database.AddParameter("@DateOfBirth", thisStaff.DOB);
+            database.AddParameter("@MobileNumber", thisStaff.MobileNumber);
+            database.AddParameter("@Position", thisStaff.Position);
+
+            return database.Execute("sproc_StaffTable_Insert");
+        }
+
+        public void update()
+        {
+            //updates user data on the database
+            clsDataConnection database = new clsDataConnection();
+
+            database.AddParameter("@FirstName", thisStaff.FirstName);
+            database.AddParameter("@Surname", thisStaff.Surname);
+            database.AddParameter("@DateOfBirth", thisStaff.DOB);
+            database.AddParameter("@MobileNumber", thisStaff.MobileNumber);
+            database.AddParameter("@Position", thisStaff.Position);
+
+            //as the toDelete field is by default null, the method must consider null to be false
+            if (thisStaff.toDelete != true)
+            {
+                database.AddParameter("@toDelete", false);
+            }
+            else
+            {
+                database.AddParameter("@toDelete", true);
+            }
+
+            database.Execute("sproc_StaffTable_Update");
+        }
+
+        public void delete()
+        {
+            //need to write procedure to delete row from table
+            //just sets the toDelete value to true
+            //cannot instantly delete user from database in case they are referenced in current pending orders
+            //      which would cause an error within the database
+
+            clsDataConnection database = new clsDataConnection();
+
+            database.AddParameter("@FirstName", thisStaff.FirstName);
+            database.AddParameter("@Surname", thisStaff.Surname);
+            database.AddParameter("@DateOfBirth", thisStaff.DOB);
+            database.AddParameter("@MobileNumber", thisStaff.MobileNumber);
+            database.AddParameter("@Position", thisStaff.Position);
+            database.AddParameter("@toDelete", true);
+
+            database.Execute("sproc_StaffTable_Update");
+
+        }
+
+    }
+}
